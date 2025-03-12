e145cb01912ba273c3e4993b99848db5e759e1a0
BeSmile <l735465519@gmail.com>
2025-03-12 14:05:57 +0800

    feat: add device

---
+++ Surge/Sgmodule/kk/bypass.js
@@ -0,0 +1,29 @@
+// modify_response.js
+
+// 获取请求的URL
+let url = $request.url;
+
+// 获取响应体
+let body = $response.body;
+
+try {
+  // 解析JSON响应体
+  let obj = JSON.parse(body);
+  // $logger.log(`Parsed JSON: ${JSON.stringify(obj)}`);
+
+  // 检查并删除 'events' 字段
+  if (obj && obj.data && 'deviceActivateType' in obj.data) {
+    obj.data.deviceActivateType = 1;
+  }
+
+  // 将修改后的对象转回字符串
+  let modifiedBody = JSON.stringify(obj);
+  // $notification.post(`Modified Body: ${modifiedBody}`);
+
+  // 设置新的响应体
+  $done({ body: modifiedBody });
+} catch (e) {
+  // 如果解析失败，记录错误并返回原始响应
+  $notification.post(`Error parsing JSON: ${e}`);
+  $done({});
+}
